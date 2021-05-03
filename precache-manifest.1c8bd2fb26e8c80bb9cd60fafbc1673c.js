22d3411342de2c18d210e39e3dd8a3309a057ada
alexamistad <alex.amistad@betterask.erni>
2021-05-03 21:30:18 +0800

    update

---
+++ precache-manifest.1c8bd2fb26e8c80bb9cd60fafbc1673c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51e7ca8e798cb18c5576c36af834675",
+    "url": "https://heartseeker.github.io/react-pwa/index.html"
+  },
+  {
+    "revision": "1f66bc54a450e74ea08c",
+    "url": "https://heartseeker.github.io/react-pwa/static/css/2.88515818.chunk.css"
+  },
+  {
+    "revision": "529874b470a908e2dd45",
+    "url": "https://heartseeker.github.io/react-pwa/static/css/main.9db7dcfc.chunk.css"
+  },
+  {
+    "revision": "1f66bc54a450e74ea08c",
+    "url": "https://heartseeker.github.io/react-pwa/static/js/2.84e9e8e6.chunk.js"
+  },
+  {
+    "revision": "a12b327e34a2c61f0dde3dee7bbd5fca",
+    "url": "https://heartseeker.github.io/react-pwa/static/js/2.84e9e8e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "529874b470a908e2dd45",
+    "url": "https://heartseeker.github.io/react-pwa/static/js/main.5046666e.chunk.js"
+  },
+  {
+    "revision": "bc6a491345e207cd90e7",
+    "url": "https://heartseeker.github.io/react-pwa/static/js/runtime-main.37c6c2c6.js"
+  },
+  {
+    "revision": "28a5af6d2cad9d22242295937ca50563",
+    "url": "https://heartseeker.github.io/react-pwa/static/media/anon.28a5af6d.png"
+  },
+  {
+    "revision": "fba1aa518b392aff3eb9462bfe395eb7",
+    "url": "https://heartseeker.github.io/react-pwa/static/media/balk.fba1aa51.png"
+  },
+  {
+    "revision": "bf14b008be2c4fcca9848b60871c020f",
+    "url": "https://heartseeker.github.io/react-pwa/static/media/dashboard-balk.bf14b008.png"
+  },
+  {
+    "revision": "265733e11590d81c6345d01fe42f9339",
+    "url": "https://heartseeker.github.io/react-pwa/static/media/google-logo.265733e1.jpg"
+  },
+  {
+    "revision": "b51a969df9c3c0c3070b22c0181233f3",
+    "url": "https://heartseeker.github.io/react-pwa/static/media/logo.b51a969d.jpeg"
+  },
+  {
+    "revision": "f286c25bb13a7d18fc41051d3a0fbfe7",
+    "url": "https://heartseeker.github.io/react-pwa/static/media/teddy-bear.f286c25b.png"
+  },
+  {
+    "revision": "e5e6b944c1863860f0e742629485a637",
+    "url": "https://heartseeker.github.io/react-pwa/static/media/uitleg_menu.e5e6b944.png"
+  }
+]);
